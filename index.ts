b1b6982daad828eacbf05d1c52a9b87ff81273a1
Arthur3kamf <manhaarthur65@gmail.com>
2024-11-14 21:38:34 -0300

    adicionando ativadade de calculo AMA CKDA

---
+++ index.ts
@@ -0,0 +1,14 @@
+let abates : number = 0;
+let assistencias : number = 0;
+let mortes : number = 0;
+let resultado : number = 0;
+
+abates=Number(prompt("digite o numero de abates: "));
+assistencias=Number(prompt("digite o numero de assistencias: "));
+mortes=Number(prompt("digite o numero de mortes: "));
+
+resultado = (abates + assistencias) / mortes;
+
+console.log(`o resultado de sua partida foi ${resultado}`);
+
+
